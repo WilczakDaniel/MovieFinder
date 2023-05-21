737d10554044a66374f4e7023028f1c05fc65892
Daniel Wilczak <d.wilczak@youlead.pl>
2023-05-21 12:13:36 +0200

    Add services and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {MovieComponent} from "./movie/movie.component";
+import {SearchComponent} from "./search/search.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'movie/:id', component: MovieComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieComponent } from './movie/movie.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    HomeComponent,
+    SearchComponent,
+    MovieListComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
